a9ed8a12620536d579db30f01dcdc3dd1dba2988
timlindeberg <tim.lindeberg@gmail.com>
2014-05-20 23:20:28 +0200

    Added imposter script an updated CaptureCharacter to use only 180 degrees

---
+++ Assets/Imposter.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Imposter : MonoBehaviour {
+	public GameObject parent;
+	public float FPS;
+	public int numberOfFrames = 15;
+	public int numberOfAngles = 8;
+	private Texture[] textures;
+	private Texture[] normalMaps;
+	private float frameTime;
+	private int frame;
+	private Mesh quad;
+	private Transform cameraTransform;
+	private int currentAngleIndex;
+	private int currentDirection;
+	private const int RIGHT = 0;
+	private const int LEFT = 1;
+
+	// Use this for initialization
+	void Start () {
+		SetUVs ();
+		GetTextures ();
+		cameraTransform = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateRotation ();
+		UpdateAnimation ();
+		LookAtCamera ();
+	}
+
+	private void LookAtCamera () {
+		Vector3 cPos = cameraTransform.position;
+		Vector3 tPos = transform.position;
+		transform.LookAt (new Vector3 (cPos.x, cPos.y, cPos.z));
+		transform.Rotate (new Vector3 (0, 180f, 0));
+	}
+
+	public void UpdateAnimation () {
+		frameTime += Time.deltaTime;
+		if (frameTime > (1f / FPS)) {
+			frame++;
+			if (frame >= numberOfFrames) {
+				frame = 0;
+			}
+			SetFrame (frame);
+			frameTime = 0;
+		}
+	}
+
+	public void UpdateRotation () {
+		Vector3 parentForward = parent.transform.forward;
+		Vector3 cameraToObject = parent.transform.position - cameraTransform.position;
+		cameraToObject.y = parentForward.y = 0;
+		float angle = 180f - Vector3.Angle (cameraToObject, parentForward);
+		int index = Mathf.RoundToInt ((angle / 180f) * (numberOfAngles - 1));
+		if (index != currentAngleIndex) {
+			// Only update when we need to
+			renderer.material.SetTexture ("_MainTex", textures [index]);
+			renderer.material.SetTexture ("_BumpMap", normalMaps [index]);
+			currentAngleIndex = index;
+			Vector3 parentRight = parent.transform.right;
+			parentRight.y = 0;
+			float dot = Vector3.Dot (cameraToObject, parentRight);
+			if (((dot > 0f) && currentDirection == LEFT) ||
+				((dot < 0f) && currentDirection == RIGHT)) {
+				// If the dot product is positive the camera is to the right of the player
+				// and vice versa. If thats true and current direction is the other way we
+				// flip the UVs.
+				FlipUVs ();
+				currentDirection = (currentDirection + 1) % 2;
+			}
+		}
+
+	}
+
+	public void FlipUVs () {
+		int n = quad.uv.Length;
+		Vector2[] uvs = new Vector2[n];
+		for (int i = 0; i < n; i++) {
+			uvs [i] = quad.uv [(i + 2) % n];
+		}
+		quad.uv = uvs;
+	}
+
+	public int GetFrame () {
+		Vector2 offset = renderer.material.GetTextureOffset ("_MainTex");
+		return (int)((offset.x / 0.25f) + (4f * offset.y / -0.25f));
+	}
+
+	public void SetFrame (int frameNumber) {
+		float x = (0.25f * (frameNumber % 4));
+		float y = (-0.25f * (frameNumber / 4));
+		Vector2 offset = new Vector2 (x, y);
+		renderer.material.SetTextureOffset ("_MainTex", offset);
+		renderer.material.SetTextureOffset ("_BumpMap", offset);
+	}
+
+	public float GetAnimationPercentage () {
+		return ((float)frame / (float)numberOfFrames);
+	}
+
+	public void SetAnimationPercentage (float percent) {
+		SetFrame (Mathf.RoundToInt (percent * numberOfFrames));
+	}
+
+	private void SetUVs () {
+		quad = gameObject.GetComponent<MeshFilter> ().mesh;
+		quad.uv = new Vector2[] {
+			new Vector2 (0f, 0.75f),
+			new Vector2 (0.25f, 1f),
+			new Vector2 (0.25f, 0.75f),
+			new Vector2 (0f, 1f)
+		};
+	}
+
+	private void GetTextures () {
+		textures = new Texture[numberOfAngles];
+		normalMaps = new Texture[numberOfAngles];
+		for (int i = 0; i < numberOfAngles; i++) {
+			textures [i] = (Texture)Resources.Load ("GeneratedTextures/texture" + i);
+			normalMaps [i] = (Texture)Resources.Load ("GeneratedTextures/normal" + i);
+		}
+		renderer.material.SetTexture ("_MainTex", textures [0]);
+		renderer.material.SetTexture ("_BumpMap", normalMaps [0]);
+	}
+}
--- Assets/Scripts/CaptureCharacter.cs
+++ Assets/Scripts/CaptureCharacter.cs
@@ -2,99 +2,106 @@
 using System.Collections;
 using System.IO;
 
-public class CaptureCharacter : MonoBehaviour {
-    public Texture2D charTexture;
-    public Texture2D normalMap;
-    public int textureSize = 1024;
-    public int numberOfAngles = 15;
-    public int numberOfFrames = 15;
+public class CaptureCharacter : MonoBehaviour
+{
+	public Texture2D charTexture;
+	public Texture2D normalMap;
+	public int textureSize = 1024;
+	public int numberOfAngles = 15;
+	public int numberOfFrames = 15;
 
-    public bool CaptureFinished { get; set; }
+	public bool CaptureFinished { get; set; }
 
-    public Texture2D[] Textures { get; set; }
+	public Texture2D[] Textures { get; set; }
 
-    public Texture2D[] NormalMaps { get; set; }
+	public Texture2D[] NormalMaps { get; set; }
     
-    private Color normalBGColor;
-    private int frameSize;
+	private Color normalBGColor;
+	private int frameSize;
 
-    private int frame = 0;
-    private int index = 0;
-    private int state;
-    private const int TEXTURE = 0;
-    private const int NORMAL = 1;
-    private Animator animator;
-    private Transform myTransform;
-    private float currentRotation = 0f;
-    private float currentNormalizedTime = 0f;
-    private float startTime;
+	private int frame = 0;
+	private int index = 0;
+	private int state;
+	private const int TEXTURE = 0;
+	private const int NORMAL = 1;
+	private Animator animator;
+	private Transform myTransform;
+	private float currentRotation = 0f;
+	private float currentNormalizedTime = 0f;
+	private float startTime;
 
-    void Start () {   
-        animator = gameObject.GetComponent<Animator> ();
-        animator.speed = 0f;
-        myTransform = gameObject.transform;
-        frameSize = textureSize / 4;
-        Textures = new Texture2D[numberOfAngles];
-        NormalMaps = new Texture2D[numberOfAngles];
-        for (int i = 0; i < Textures.Length; i++) {
-            Textures [i] = new Texture2D (textureSize, textureSize, TextureFormat.ARGB32, false);
-            NormalMaps [i] = new Texture2D (textureSize, textureSize, TextureFormat.ARGB32, false);
-        }
-        normalBGColor = new Color (127f, 127f, 254f, 255f) / 255f;
-        Camera.main.backgroundColor = Color.clear;
-        state = TEXTURE;
-        renderer.material.mainTexture = charTexture;
-        StartCoroutine (CaptureFrames ());
-    }
+	void Start ()
+	{   
+		animator = gameObject.GetComponent<Animator> ();
+		animator.speed = 0f;
+		myTransform = gameObject.transform;
+		frameSize = textureSize / 4;
+		Textures = new Texture2D[numberOfAngles];
+		NormalMaps = new Texture2D[numberOfAngles];
+		for (int i = 0; i < Textures.Length; i++) {
+			Textures [i] = new Texture2D (textureSize, textureSize, TextureFormat.ARGB32, false);
+			NormalMaps [i] = new Texture2D (textureSize, textureSize, TextureFormat.ARGB32, false);
+		}
+		normalBGColor = new Color (127f, 127f, 254f, 255f) / 255f;
+		Camera.main.backgroundColor = Color.clear;
+		state = TEXTURE;
+		renderer.material.mainTexture = charTexture;
+		StartCoroutine (CaptureFrames ());
+	}
     
-    IEnumerator CaptureFrames () {
-        startTime = Time.time;
-        for (index = 0; index < numberOfAngles; index++) {
-            while (frame < numberOfFrames) {
-                yield return new WaitForEndOfFrame ();
-                Capture ((state == TEXTURE) ? Textures : NormalMaps);
-                if (state == NORMAL) {
-                    UpdateAnimation ();
-                    frame++;
-                }
-                state = (state + 1) % 2;
-                renderer.material.mainTexture = (state == TEXTURE) ? charTexture : normalMap;
-                Camera.main.backgroundColor = (state == TEXTURE) ? Color.clear : normalBGColor;
-            }
-            RotateObject ();
-            SaveToPNG ("texture" + index + ".png", Textures [index]);
-            SaveToPNG ("normal" + index + ".png", NormalMaps [index]);
-            frame = 0;
-        }
-        CaptureFinished = true;
-        Debug.Log ("Time to generate imposter: " + (Time.time - startTime) + " seconds");
-    }
+	private IEnumerator CaptureFrames ()
+	{
+		startTime = Time.time;
+		for (index = 0; index < numberOfAngles; index++) {
+			while (frame < numberOfFrames) {
+				yield return new WaitForEndOfFrame ();
+				Capture ((state == TEXTURE) ? Textures : NormalMaps);
+				if (state == NORMAL) {
+					UpdateAnimation ();
+					frame++;
+				}
+				state = (state + 1) % 2;
+				renderer.material.mainTexture = (state == TEXTURE) ? charTexture : normalMap;
+				Camera.main.backgroundColor = (state == TEXTURE) ? Color.clear : normalBGColor;
+			}
+			RotateObject ();
+			SaveToPNG ("texture" + index + ".png", Textures [index]);
+			SaveToPNG ("normal" + index + ".png", NormalMaps [index]);
+			frame = 0;
+		}
+		CaptureFinished = true;
+		Debug.Log ("Time to generate imposter: " + (Time.time - startTime) + " seconds");
+	}
     
-    // Return part of the screen in a texture.
-    public void Capture (Texture2D[] tex) {
-        int rows = textureSize / frameSize;
-        int x = frameSize * (frame % rows);
-        int y = textureSize - frameSize * ((frame / rows) + 1);
-        Texture2D newTexture = new Texture2D (Screen.width, Screen.height, TextureFormat.ARGB32, false);
-        newTexture.ReadPixels (new Rect (0f, 0f, Screen.width, Screen.height), 0, 0, false);
-        TextureScale.Bilinear (newTexture, frameSize, frameSize);   
-        tex [index].SetPixels (x, y, frameSize, frameSize, newTexture.GetPixels ());
-        tex [index].Apply (false);
-    }
+	// Return part of the screen in a texture.
+	private void Capture (Texture2D[] tex)
+	{
+		int rows = textureSize / frameSize;
+		int x = frameSize * (frame % rows);
+		int y = textureSize - frameSize * ((frame / rows) + 1);
+		Texture2D newTexture = new Texture2D (Screen.width, Screen.height, TextureFormat.ARGB32, false);
+		newTexture.ReadPixels (new Rect (0f, 0f, Screen.width, Screen.height), 0, 0, false);
+		TextureScale.Bilinear (newTexture, frameSize, frameSize);   
+		tex [index].SetPixels (x, y, frameSize, frameSize, newTexture.GetPixels ());
+		tex [index].Apply (false);
+	}
     
-    public static void SaveToPNG (string name, Texture2D tex) {
-        File.WriteAllBytes (Application.dataPath + "/GeneratedTextures/" + name, tex.EncodeToPNG ());
-    }
+	public static void SaveToPNG (string name, Texture2D tex)
+	{
+		File.WriteAllBytes (Application.dataPath + "/Resources/GeneratedTextures/" + name, tex.EncodeToPNG ());
+	}
 
-    void UpdateAnimation () {
-        currentNormalizedTime += (float)(1f / numberOfFrames);
-        animator.ForceStateNormalizedTime (currentNormalizedTime);
-        myTransform.position = new Vector3 (0f, 0f, -2f);
-    }
+	private void UpdateAnimation ()
+	{
+		currentNormalizedTime += (float)(1f / numberOfFrames);
+		animator.ForceStateNormalizedTime (currentNormalizedTime);
+		myTransform.position = new Vector3 (0f, 0f, -2f);
+	}
 
-    void RotateObject () {
-        currentRotation += (float)(360f / numberOfAngles);
-        gameObject.transform.eulerAngles = new Vector3 (0, currentRotation, 0);
-        currentNormalizedTime = 0f;
-    }
+	private void RotateObject ()
+	{
+		currentRotation += (float)(180f / numberOfAngles);
+		gameObject.transform.eulerAngles = new Vector3 (0, currentRotation, 0);
+		currentNormalizedTime = 0f;
+	}
 }
