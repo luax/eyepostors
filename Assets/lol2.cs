2f652390ef5b98a74ea85a2b6d1445d878dda314
cookieluv <mart.schon@gmail.com>
2014-05-20 17:23:21 +0200

    Updated CaptureCharacter, added GenerateImpostor scene and added some small test scrips

---
+++ Assets/lol2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lol2 : MonoBehaviour {
+    float currentX;
+    // Use this for initialization
+    void Start () {
+	
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        currentX += 1f * Time.deltaTime;
+        transform.Rotate (new Vector3 (currentX, 0, 0));
+        if (currentX > 10f) {
+            currentX = 0f;
+        }
+    }
+}
