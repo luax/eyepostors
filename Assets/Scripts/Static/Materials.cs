9c3d4aaaf6275063ab276515c34bfe611b36a46e
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-05-21 11:14:59 +0200

    Added a static materials class

---
+++ Assets/Scripts/Static/Materials.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Materials : Singleton<Materials>
+{
+    public int numberOfAngles = 8;
+    private Material[] materials;
+
+    void Awake()
+    {
+        materials = new Material[numberOfAngles];
+        for (int i = 0; i < numberOfAngles; i++)
+        {
+            materials[i] = MakeMaterial(i);
+        }
+    }
+
+    public Material GetMaterial(int index)
+    {
+        return materials[index];
+    }
+
+    private Material MakeMaterial(int index)
+    {
+        Material mat = new Material(Shader.Find("Transparent/Cutout/Bumped Diffuse"));
+        mat.SetTexture("_MainTex", (Texture)Resources.Load("GeneratedTextures/texture" + index));
+        mat.SetTexture("_BumpMap", (Texture)Resources.Load("GeneratedTextures/normal" + index));
+        return mat;
+    }
+}
