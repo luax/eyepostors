cfe9f2a08cba4f22d9134eb4951ed1cf1111ec13
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-05-21 16:32:04 +0200

    Made gaze distance use a projected rectangle and change LOD accordingly

--- Assets/Scripts/CharacterGazeLOD.cs
+++ Assets/Scripts/CharacterGazeLOD.cs
@@ -3,107 +3,122 @@ using System.Collections;
 
 public enum TriggerOption
 {
-	Mouse,
-	Gaze
+    Mouse,
+    Gaze
 }
 
 public class CharacterGazeLOD : MonoBehaviour
 {
-	public TriggerOption option;
-	public float highLimit = 0.2f;
-	public float standardLimit = 0.5f;
-	public float maxDistance = 10f;
-	public Mesh high;
-	public Mesh medium;
-	public GameObject impostor;
-	public GameObject characterMesh;
+    public TriggerOption option;
+    public float highLimit = 50f; // TODO
+    public float standardLimit = 180f; // TODO
+    public float maxDistance = 10f;
+    public Mesh high;
+    public Mesh medium;
+    public GameObject impostor;
+    public GameObject characterMesh;
 
-	private Transform myTransform;
-	private Transform cameraTransform;
-	private SkinnedMeshRenderer characterRenderer;
-	private LOD currentLOD;
-	private Impostor impostorScript;
-	
-	private enum LOD
-	{
-		High,
-		Standard,
-		Low
-	}
+    private Transform myTransform;
+    private Transform cameraTransform;
+    private SkinnedMeshRenderer characterRenderer;
+    private LOD currentLOD;
+    private Impostor impostorScript;
 
-	public void Awake ()
-	{
-		myTransform = transform;
-		cameraTransform = Camera.main.transform;
-		characterRenderer = characterMesh.GetComponent<SkinnedMeshRenderer> ();
-		impostorScript = impostor.GetComponent<Impostor> ();
-		characterMesh.SetActive (false);
-		impostor.SetActive (true);
-	}
+    private enum LOD
+    {
+        High,
+        Standard,
+        Low
+    }
 
-	public void Update ()
-	{
-		Vector3 cPos = cameraTransform.position;
-		Vector3 tPos = myTransform.position;
-		cPos.y = tPos.y = 0;
-		float distance = Vector3.Distance (cPos, tPos);
-		
-		if (distance > maxDistance) {
-			SetLOD (LOD.Low);
-			return;
-		}
+    public void Awake()
+    {
+        myTransform = transform;
+        cameraTransform = Camera.main.transform;
+        characterRenderer = characterMesh.GetComponent<SkinnedMeshRenderer>();
+        impostorScript = impostor.GetComponent<Impostor>();
+        characterMesh.SetActive(false);
+        impostor.SetActive(true);
+    }
 
-		distance = GazeDistance.Instance.CalculateDistance (transform.position);
-		
-		SetLOD (GetLOD (distance));
-	}
-	
-	private void SetLOD (LOD lod)
-	{
-		if (currentLOD == lod) {
-			return;
-		}
-		switch (lod) {
-		case LOD.High:
-			SetImpostor (false);
-			SetCharacterMesh (true, high);
-			break;
-		case LOD.Standard:
-			SetImpostor (false);
-			SetCharacterMesh (true, medium);
-			break;
-		case LOD.Low:
-			SetImpostor (true);
-			SetCharacterMesh (false);
-			break;
-		}
-		currentLOD = lod;
-	}
+    public void Update()
+    {
+        Vector3 cPos = cameraTransform.position;
+        Vector3 tPos = myTransform.position;
+        cPos.y = tPos.y = 0;
+        float distance = Vector3.Distance(cPos, tPos);
 
-	private LOD GetLOD (float distance)
-	{
-		if (distance < highLimit) {
-			return LOD.High;
-		} else if (distance < standardLimit) {
-			return LOD.Standard;
-		}
-		return LOD.Low;
-	}
+        if (distance > maxDistance)
+        {
+            SetLOD(LOD.Low);
+            return;
+        }
+
+        if (impostor.activeSelf)
+        {
+            distance = GazeDistance.Instance.CalculateDistance(impostor, option);
+        }
+        else
+        {
+            distance = GazeDistance.Instance.CalculateDistance(characterMesh, option);
+        }
+
+        SetLOD(GetLOD(distance));
+    }
+
+    private void SetLOD(LOD lod)
+    {
+        if (currentLOD == lod)
+        {
+            return;
+        }
+        switch (lod)
+        {
+            case LOD.High:
+                SetImpostor(false);
+                SetCharacterMesh(true, high);
+                break;
+            case LOD.Standard:
+                SetImpostor(false);
+                SetCharacterMesh(true, medium);
+                break;
+            case LOD.Low:
+                SetImpostor(true);
+                SetCharacterMesh(false);
+                break;
+        }
+        currentLOD = lod;
+    }
+
+    private LOD GetLOD(float distance)
+    {
+        if (distance < highLimit)
+        {
+            return LOD.High;
+        }
+        else if (distance < standardLimit)
+        {
+            return LOD.Standard;
+        }
+        return LOD.Low;
+    }
 
 
-	private void SetImpostor (bool active)
-	{
-		impostor.SetActive (active);
-		if (active) {
-			impostorScript.Update ();
-		}
-	}
+    private void SetImpostor(bool active)
+    {
+        impostor.SetActive(active);
+        if (active)
+        {
+            impostorScript.Update();
+        }
+    }
 
-	private void SetCharacterMesh (bool active, Mesh mesh = null)
-	{
-		characterMesh.SetActive (active);
-		if (active && mesh) {
-			characterRenderer.sharedMesh = mesh;
-		}
-	}
+    private void SetCharacterMesh(bool active, Mesh mesh = null)
+    {
+        characterMesh.SetActive(active);
+        if (active && mesh)
+        {
+            characterRenderer.sharedMesh = mesh;
+        }
+    }
 }
--- Assets/Scripts/Static/GazeDistance.cs
+++ Assets/Scripts/Static/GazeDistance.cs
@@ -4,13 +4,29 @@ using System.Collections;
 public class GazeDistance : Singleton<GazeDistance>
 {
     public EyeXGazePointType gazePointType = EyeXGazePointType.GazeLightlyFiltered;
+    public TriggerOption triggerOption = TriggerOption.Mouse;
 
     private EyeXGazePointProvider gazePointProvider;
     private EyeXGazePoint gazePoint;
-    
+
+    // For debugging
+    public const bool DEBUG = false;
+    private Material red;
+    private Material blue;
+    private GameObject latestObject;
+
+
     public void Awake()
     {
         gazePointProvider = EyeXGazePointProvider.GetInstance();
+    }
+
+    public void Start()
+    {
+        if (DEBUG)
+        {
+            latestObject = gameObject;
+        }
     }
 
     public void OnEnable()
@@ -28,15 +44,51 @@ public class GazeDistance : Singleton<GazeDistance>
         gazePoint = gazePointProvider.GetLastGazePoint(gazePointType);
     }
 
-    public float CalculateDistance(Vector3 position)
+    public float CalculateDistance(GameObject gameObject, TriggerOption option)
     {
         if (!gazePoint.IsValid || !gazePoint.IsWithinScreenBounds)
         {
             return float.MaxValue;
         }
-        position = Camera.main.WorldToScreenPoint(position);
-        Vector2 gaze = new Vector2(gazePoint.Screen.x / Screen.width, gazePoint.Screen.y / Screen.height);
-        Vector2 pos = new Vector2(position.x / Screen.width, position.y / Screen.height);
-        return Vector2.Distance(gaze, pos);
+
+        if (DEBUG)
+        {
+            latestObject = gameObject;
+        }
+
+        Rect rect = ProjectedRect.GetProjectedRect(gameObject, Camera.main).rect;
+
+        if (option == TriggerOption.Mouse)
+        {
+            return DistanceToRectangle(rect, Input.mousePosition);
+        }
+        else
+        {
+            return DistanceToRectangle(rect, gazePoint.GUI);
+        }
+    }
+
+
+    private float DistanceToRectangle(Rect rect, Vector2 gaze)
+    {
+        float dx = Mathf.Max(rect.xMin - gaze.x, 0, gaze.x - rect.xMax);
+        float dy = Mathf.Max(rect.yMin - gaze.y, 0, gaze.y - rect.yMax);
+        return Mathf.Sqrt(dx * dx + dy * dy);
+    }
+
+    public void OnGUI()
+    {
+        if (DEBUG && latestObject.renderer != null)
+        {
+            GUI.Box(ProjectedRect.GetProjectedRect(latestObject, Camera.main).rect, "Distance from trigger option " + CalculateDistance(latestObject, triggerOption));
+        }
+    }
+
+    // Tobii version is probably better than this
+    public Rect BoundsToScreenRect(Bounds bounds)
+    {
+        Vector3 origin = Camera.main.WorldToScreenPoint(new Vector3(bounds.min.x, bounds.max.y, bounds.max.z));
+        Vector3 extent = Camera.main.WorldToScreenPoint(new Vector3(bounds.max.x, bounds.min.y, bounds.min.z));
+        return new Rect(origin.x, Screen.height - origin.y, extent.x - origin.x, origin.y - extent.y);
     }
 }
