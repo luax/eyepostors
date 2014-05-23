e0b6a42e5b16ccbb66e413d4591adf369f819212
Tim Lindeberg <tim.lindeberg@gmail.com>
2014-05-23 15:46:11 +0200

    Capture character now captures vertical views and generates proper normals

--- Assets/Scripts/Generators/CaptureCharacter.cs
+++ Assets/Scripts/Generators/CaptureCharacter.cs
@@ -12,10 +12,11 @@ public class CaptureCharacter : MonoBehaviour
 
 	public bool CaptureFinished { get; set; }
 
-	public Texture2D[] Textures { get; set; }
+	public Texture2D[,] Textures { get; set; }
 
-	public Texture2D[] NormalMaps { get; set; }
+	public Texture2D[,] NormalMaps { get; set; }
     
+	private Transform cameraPivot;
 	private Animator animator;
 	private Transform myTransform;
 	private Transform midGeoTransform;
@@ -24,138 +25,158 @@ public class CaptureCharacter : MonoBehaviour
 	private int textureSizeVertical;
 	private int frameSizeVertical;
 	private int frameSizeHorizontal;
+	private int totalFrames;
 	private int frame = 0;
-	private int index = 0;
+	private int indexY = 0;
+	private int indexX = 0;
 	private float currentRotation = 0f;
 	private float currentNormalizedTime = 0f;
 	private float startTime;
 	private Color[] blankFrame;
 	private Color[] blankNormalFrame;
 
-	void Start()
+	void Start ()
 	{   	
 		myTransform = gameObject.transform;
-		midGeoTransform = myTransform.FindChild("CarlMidGeo");
-		myRenderer = midGeoTransform.gameObject.GetComponent<SkinnedMeshRenderer>();
-		normalGenerator = GetComponent<GenerateNormals>();
-		animator = GetComponent<Animator>();
+		midGeoTransform = transform.FindChild ("CarlMidGeo");
+		myRenderer = midGeoTransform.GetComponent<SkinnedMeshRenderer> ();
+		cameraPivot = GameObject.Find ("CameraPivot").transform;
+		normalGenerator = GetComponent<GenerateNormals> ();
+		animator = GetComponent<Animator> ();
 		animator.speed = 0f;
 		
 		textureSizeVertical = textureSizeHorizontal / 2;
 		frameSizeHorizontal = textureSizeHorizontal / 8;
 		frameSizeVertical = textureSizeVertical / 2;
-		Textures = new Texture2D[numberOfAngles];
-		NormalMaps = new Texture2D[numberOfAngles];
+		int framesInRow = textureSizeHorizontal / frameSizeHorizontal;
+		int framesInColumn = textureSizeVertical / frameSizeVertical;
+		totalFrames = framesInRow * framesInColumn;
+		
+		Textures = new Texture2D[numberOfAngles, numberOfAngles];
+		NormalMaps = new Texture2D[numberOfAngles, numberOfAngles];
 		
 		int numPixels = frameSizeHorizontal * frameSizeVertical;
 		blankFrame = new Color[numPixels];
 		blankNormalFrame = new Color[numPixels];
-		Color normalColor = new Color(0.5f, 0.5f, 1f, 1f);
-		for (int i = 0; i < numPixels; i++)
-		{
+		Color normalColor = new Color (0.5f, 0.5f, 1f, 1f);
+		for (int i = 0; i < numPixels; i++) {
 			blankFrame [i] = Color.clear;
 			blankNormalFrame [i] = normalColor;
 		}
 		
-		for (int i = 0; i < Textures.Length; i++)
-		{
-			Textures [i] = new Texture2D(textureSizeHorizontal, textureSizeVertical, TextureFormat.ARGB32, false);
-			NormalMaps [i] = new Texture2D(textureSizeHorizontal, textureSizeVertical, TextureFormat.ARGB32, false);
+		for (int x = 0; x < numberOfAngles; x++) {
+			for (int y = 0; y < numberOfAngles; y++) {
+				Textures [x, y] = new Texture2D (textureSizeHorizontal, textureSizeVertical, TextureFormat.ARGB32, false);
+				NormalMaps [x, y] = new Texture2D (textureSizeHorizontal, textureSizeVertical, TextureFormat.ARGB32, false);
+			}
 		}
 		Camera.main.backgroundColor = Color.clear;
 		myRenderer.material.mainTexture = charTexture;
-		StartCoroutine(CaptureFrames());
+		StartCoroutine (CaptureFrames ());
 	}
     
-	private IEnumerator CaptureFrames()
+	private IEnumerator CaptureFrames ()
 	{
 		startTime = Time.time;
-		int framesInRow = textureSizeHorizontal / frameSizeHorizontal;
-		int framesInColumn = textureSizeVertical / frameSizeVertical;
-		int totalFrames = framesInRow * framesInColumn;
-		for (index = 0; index < numberOfAngles; index++)
-		{
-			for (frame = 0; frame < numberOfFrames; frame++)
-			{
-				yield return new WaitForEndOfFrame();
-				Capture();
-				UpdateAnimation();
+		Vector3 yAxis = new Vector3 (0f, 1f, 0f);
+		Vector3 xAxis = new Vector3 (1f, 0f, 0f);
+		for (indexX = 0; indexX < numberOfAngles; indexX++) {
+			for (indexY = 0; indexY < numberOfAngles; indexY++) {
+				for (frame = 0; frame < numberOfFrames; frame++) {
+					yield return new WaitForEndOfFrame ();
+					Capture ();
+					UpdateAnimation ();
+				}
+				FillTextures ();
+				SaveTextures ();
+				RotateCamera (yAxis, 360f);
+				frame = 0;
+				Debug.Log ("x: " + indexX + ", y: " + indexY);
 			}
-			
-			//Fill rest of image with blank pixels
-			for (int i = frame; i < totalFrames; i++)
-			{
-				FillBlankFrame(i, Textures [index], blankFrame);
-				FillBlankFrame(i, NormalMaps [index], blankNormalFrame);
-			}
-			
-			RotateObject();
-			if (saveTextures)
-			{
-				SaveToPNG("texture" + index + ".png", Textures [index]);
-				SaveToPNG("normal" + index + ".png", NormalMaps [index]);
-			}
-			frame = 0;
+			ResetYRotation ();
+			currentRotation = 0;
+			indexY = 0;
+			RotateCamera (xAxis, 90f);
 		}
 		CaptureFinished = true;
-		Debug.Log("Time to generate imposter: " + (Time.time - startTime) + " seconds");
+		Debug.Log ("Time to generate imposter: " + (Time.time - startTime) + " seconds");
+	}
+	
+	private void FillTextures ()
+	{
+		for (int i = frame; i < totalFrames; i++) {
+			FillBlankFrame (i, Textures [indexX, indexY], blankFrame);
+			FillBlankFrame (i, NormalMaps [indexX, indexY], blankNormalFrame);
+		}
+	}
+	
+	private void SaveTextures ()
+	{
+		if (saveTextures) {
+			SaveToPNG ("texture" + indexX + "_" + indexY + ".png", Textures [indexX, indexY]);
+			SaveToPNG ("normal" + indexX + "_" + indexY + ".png", NormalMaps [indexX, indexY]);
+		}
 	}
     
 	// Return part of the screen in a texture.
-	private void Capture()
+	private void Capture ()
 	{
-		Texture2D textureFrame = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
-		textureFrame.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0, false);
-		Texture2D normalFrame = normalGenerator.GetNormal();
-		SetFrameInTexture(Textures [index], textureFrame);
-		SetFrameInTexture(NormalMaps [index], normalFrame);
+		Texture2D textureFrame = new Texture2D (Screen.width, Screen.height, TextureFormat.ARGB32, false);
+		textureFrame.ReadPixels (new Rect (0f, 0f, Screen.width, Screen.height), 0, 0, false);
+		Texture2D normalFrame = normalGenerator.GetNormal ();
+		SetFrameInTexture (Textures [indexX, indexY], textureFrame);
+		SetFrameInTexture (NormalMaps [indexX, indexY], normalFrame);
 	}
 	
-	private void FillBlankFrame(int frame, Texture2D texture, Color[] colors)
+	private void FillBlankFrame (int frame, Texture2D texture, Color[] colors)
 	{
-		Vector2 px = GetXY(frame);
+		Vector2 px = GetXY (frame);
 		int x = (int)px.x;
 		int y = (int)px.y;
-		texture.SetPixels(x, y, frameSizeHorizontal, frameSizeVertical, colors);
-		texture.Apply(false);
+		texture.SetPixels (x, y, frameSizeHorizontal, frameSizeVertical, colors);
+		texture.Apply (false);
 	}
     
-	private void SetFrameInTexture(Texture2D texture, Texture2D textureFrame)
+	private void SetFrameInTexture (Texture2D texture, Texture2D textureFrame)
 	{
-		Vector2 px = GetXY(frame);
+		Vector2 px = GetXY (frame);
 		int x = (int)px.x;
 		int y = (int)px.y;
-		TextureScale.Bilinear(textureFrame, frameSizeHorizontal, frameSizeVertical); 
-		texture.SetPixels(x, y, frameSizeHorizontal, frameSizeVertical, textureFrame.GetPixels());
-		texture.Apply(false);
+		TextureScale.Bilinear (textureFrame, frameSizeHorizontal, frameSizeVertical); 
+		texture.SetPixels (x, y, frameSizeHorizontal, frameSizeVertical, textureFrame.GetPixels ());
+		texture.Apply (false);
 	}
 	
-	private Vector2 GetXY(int frame)
+	private Vector2 GetXY (int frame)
 	{
 		int columns = textureSizeHorizontal / frameSizeHorizontal;		
 		int x = frameSizeHorizontal * (frame % columns);
 		int y = textureSizeVertical - frameSizeVertical * ((frame / columns) + 1);
-		return new Vector2(x, y);
+		return new Vector2 (x, y);
 	}
 
-	private void UpdateAnimation()
+	private void UpdateAnimation ()
 	{
 		currentNormalizedTime += (float)(1f / numberOfFrames);
-		animator.ForceStateNormalizedTime(currentNormalizedTime);
-		midGeoTransform.position = new Vector3(0f, 0f, 0f);
-		myTransform.position = new Vector3(0f, 0f, 0f);
+		animator.ForceStateNormalizedTime (currentNormalizedTime);
+		midGeoTransform.position = new Vector3 (0f, 0f, 0f);
+		myTransform.position = new Vector3 (0f, 0f, 0f);
 	}
 
-	private void RotateObject()
+	private void RotateCamera (Vector3 axis, float totalAngles)
 	{
-		currentRotation += (float)(180f / (numberOfAngles - 1));
-		midGeoTransform.localEulerAngles = -new Vector3(0, currentRotation, 0);
-		myTransform.eulerAngles = new Vector3(0, currentRotation, 0);
+		cameraPivot.eulerAngles -= (float)(totalAngles / (numberOfAngles - 1)) * axis;
 		currentNormalizedTime = 0f;
 	}
 	
-	public static void SaveToPNG(string name, Texture2D tex)
+	private void ResetYRotation ()
 	{
-		File.WriteAllBytes(Application.dataPath + "/Resources/GeneratedTextures/" + name, tex.EncodeToPNG());
+		Vector3 e = cameraPivot.eulerAngles;
+		cameraPivot.eulerAngles = new Vector3 (e.x, 0, 0);
+	}
+	
+	public static void SaveToPNG (string name, Texture2D tex)
+	{
+		File.WriteAllBytes (Application.dataPath + "/Resources/GeneratedTextures/" + name, tex.EncodeToPNG ());
 	}
 }
--- Assets/Scripts/Static/Materials.cs
+++ Assets/Scripts/Static/Materials.cs
@@ -3,28 +3,29 @@ using System.Collections;
 
 public class Materials : Singleton<Materials>
 {
-    public int numberOfAngles = 15;
-    private Material[] materials;
+	public int numberOfAngles = 16;
+	private Material[,] materials;
 
-    void Awake()
-    {
-        materials = new Material[numberOfAngles];
-        for (int i = 0; i < numberOfAngles; i++)
-        {
-            materials[i] = MakeMaterial(i);
-        }
-    }
+	void Awake ()
+	{
+		materials = new Material[numberOfAngles, numberOfAngles];
+		for (int x = 0; x < numberOfAngles; x++) {
+			for (int y = 0; y < numberOfAngles; y++) {
+				materials [x, y] = MakeMaterial (x, y);
+			}
+		}
+	}
 
-    public Material GetMaterial(int index)
-    {
-        return materials[index];
-    }
+	public Material GetMaterial (int x, int y)
+	{
+		return materials [x, y];
+	}
 
-    private Material MakeMaterial(int index)
-    {
-        Material mat = new Material(Shader.Find("Transparent/Cutout/Bumped Diffuse"));
-        mat.SetTexture("_MainTex", (Texture)Resources.Load("GeneratedTextures/texture" + index));
-        mat.SetTexture("_BumpMap", (Texture)Resources.Load("GeneratedTextures/normal" + index));
-        return mat;
-    }
+	private Material MakeMaterial (int indexX, int indexY)
+	{
+		Material mat = new Material (Shader.Find ("Transparent/Cutout/Bumped Diffuse"));
+		mat.SetTexture ("_MainTex", (Texture)Resources.Load ("GeneratedTextures/texture" + indexX + "_" + indexY));
+		mat.SetTexture ("_BumpMap", (Texture)Resources.Load ("GeneratedTextures/normal" + indexX + "_" + indexY));
+		return mat;
+	}
 }
