cc34c3ab288dd95eb1ad10406fd202e9920c9763
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-05-24 23:13:30 +0200

    Made a free view camera prefab

---
+++ Assets/Scripts/Misc/FlyController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyController : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.Translate((new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) + (Input.GetKey(KeyCode.Space) ? Vector3.up : Vector3.zero)) * Time.deltaTime * 20f);
+    }
+}
