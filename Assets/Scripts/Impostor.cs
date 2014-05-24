13ee07854004febed62932ce0a1bf2d6363d0441
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2014-05-24 11:53:33 +0200

    Made the materials class static

--- Assets/Scripts/Impostor.cs
+++ Assets/Scripts/Impostor.cs
@@ -3,110 +3,111 @@ using System.Collections;
 
 public class Impostor : MonoBehaviour
 {
-	public Color shirtColor;
+    public Color shirtColor;
 
-	// Component references
-	private Transform impostorTransform;
-	private Transform parentTransform;
-	private Transform cameraTransform;
-	private Renderer impostorRenderer;
-	private CharacterGazeLOD characterGazeLOD;
-	private Mesh quad;
-	private int frameIndex;
-	private int currentAngleIndex;
-	private int currentDirection;
-	private const int RIGHT = 0;
-	private const int LEFT = 1;
+    // Component references
+    private Transform impostorTransform;
+    private Transform parentTransform;
+    private Transform cameraTransform;
+    private Renderer impostorRenderer;
+    private CharacterGazeLOD characterGazeLOD;
+    private Mesh quad;
+    private int frameIndex;
+    private int currentAngleIndex;
+    private int currentDirection;
+    private const int RIGHT = 0;
+    private const int LEFT = 1;
 
-	void Start ()
-	{
-		impostorTransform = transform;
-		cameraTransform = Camera.main.transform;
-		parentTransform = impostorTransform.parent;
-		impostorRenderer = renderer;
-		characterGazeLOD = transform.parent.GetComponent<CharacterGazeLOD> ();
-		quad = gameObject.GetComponent<MeshFilter> ().mesh;
-		SetUVs ();
-		SetMaterial (0);
-	}
-    
-	public void Update ()
-	{
-		UpdateRotation ();
-		UpdateAnimation ();
-		LookAtCamera ();
-	}
+    void Start()
+    {
+        impostorTransform = transform;
+        cameraTransform = Camera.main.transform;
+        parentTransform = impostorTransform.parent;
+        impostorRenderer = renderer;
+        characterGazeLOD = transform.parent.GetComponent<CharacterGazeLOD>();
+        quad = gameObject.GetComponent<MeshFilter>().mesh;
+        SetUVs();
+        SetMaterial(0);
+    }
 
-	private void LookAtCamera ()
-	{
-		Vector3 cPos = cameraTransform.position;
-		Vector3 tPos = impostorTransform.position;
-		impostorTransform.LookAt (new Vector3 (cPos.x, tPos.y, cPos.z));
-		impostorTransform.Rotate (new Vector3 (0, 180f, 0));
-	}
+    public void Update()
+    {
+        UpdateRotation();
+        UpdateAnimation();
+        LookAtCamera();
+    }
 
-	public void UpdateAnimation ()
-	{
-		SetAnimationPercentage (characterGazeLOD.NormalizedTime);
-	}
+    private void LookAtCamera()
+    {
+        Vector3 cPos = cameraTransform.position;
+        Vector3 tPos = impostorTransform.position;
+        impostorTransform.LookAt(new Vector3(cPos.x, tPos.y, cPos.z));
+        impostorTransform.Rotate(new Vector3(0, 180f, 0));
+    }
 
-	public void UpdateRotation ()
-	{
-		Vector3 parentForward = parentTransform.forward;
-		Vector3 parentRight = parentTransform.right;
-		Vector3 cameraToObject = parentTransform.position - cameraTransform.position;
-		parentRight.y = cameraToObject.y = 0;
-		
-		float angle = 180f - Vector3.Angle (cameraToObject, parentForward);
+    public void UpdateAnimation()
+    {
+        SetAnimationPercentage(characterGazeLOD.NormalizedTime);
+    }
+
+    public void UpdateRotation()
+    {
+        Vector3 parentForward = parentTransform.forward;
+        Vector3 parentRight = parentTransform.right;
+        Vector3 cameraToObject = parentTransform.position - cameraTransform.position;
+        parentRight.y = cameraToObject.y = 0;
+
+        float angle = 180f - Vector3.Angle(cameraToObject, parentForward);
         int index = Mathf.RoundToInt((angle / 180f) * (Settings.numberOfAngles / 2 - 1));
         if (index != currentAngleIndex && index != Settings.numberOfAngles - currentAngleIndex)
         {
-			float dot = Vector3.Dot (cameraToObject, parentRight);
-			if (dot < 0f) {
+            float dot = Vector3.Dot(cameraToObject, parentRight);
+            if (dot < 0f)
+            {
                 index = Settings.numberOfAngles - index - 1;
-			}
-			SetMaterial (index);
-			currentAngleIndex = index;
-		}
-	}
+            }
+            SetMaterial(index);
+            currentAngleIndex = index;
+        }
+    }
 
-	public void SetFrame (int frameNumber)
-	{
-		float x = (0.125f * (frameNumber % 8));
-		float y = 0.5f - (0.5f * (frameNumber / 8));
-		quad.uv = new Vector2[] { 
+    public void SetFrame(int frameNumber)
+    {
+        float x = (0.125f * (frameNumber % 8));
+        float y = 0.5f - (0.5f * (frameNumber / 8));
+        quad.uv = new Vector2[] { 
 			new Vector2 (x, y),
 		 	new Vector2 (x + 0.125f, 0.5f + y),
 		 	new Vector2 (x + 0.125f, y),
 		 	new Vector2 (x, 0.5f + y)};
-	}
+    }
 
-	public float GetAnimationPercentage ()
-	{
+    public float GetAnimationPercentage()
+    {
         return ((float)frameIndex / (float)Settings.numberOfFrames);
-	}
+    }
 
-	public void SetAnimationPercentage (float percent)
-	{
+    public void SetAnimationPercentage(float percent)
+    {
         frameIndex = Mathf.RoundToInt(percent * Settings.numberOfFrames);
         frameIndex = (frameIndex == Settings.numberOfFrames) ? 0 : frameIndex;
-		SetFrame (frameIndex);
-	}
+        SetFrame(frameIndex);
+    }
 
-	private void SetUVs ()
-	{
-		quad.uv = new Vector2[] {
+    private void SetUVs()
+    {
+        quad.uv = new Vector2[] {
             new Vector2 (0f, 0.5f),
             new Vector2 (0.125f, 1f),
             new Vector2 (0.125f, 0.5f),
             new Vector2 (0f, 1f)
         };
-	}
+    }
 
-	private void SetMaterial (int frameIndex)
-	{
-		impostorRenderer.material = Materials.Instance.GetMaterial (0, frameIndex);
-		impostorRenderer.material.SetColor ("_ShirtColor", shirtColor);
-		SetFrame (frameIndex);
-	}
+    private void SetMaterial(int frameIndex)
+    {
+        impostorRenderer.material = Materials.GetMaterial(0, frameIndex);
+        impostorRenderer.material.SetColor("_ShirtColor", shirtColor);
+        SetFrame(frameIndex);
+    }
 }
--- Assets/Scripts/Static/Materials.cs
+++ Assets/Scripts/Static/Materials.cs
@@ -1,33 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Materials : Singleton<Materials>
+public static class Materials
 {
-	private int numberOfAngles = Settings.numberOfAngles;
-	private Material[,] materials;
-	private string shader = "ShirtColor/Transparent";
+    private static int numberOfAngles = Settings.numberOfAngles;
+    private static Material[,] materials;
+    private static string shader = "ShirtColor/Transparent";
 
-	void Awake ()
-	{
-		materials = new Material[numberOfAngles, numberOfAngles];
-		for (int x = 0; x < numberOfAngles; x++) {
-			for (int y = 0; y < numberOfAngles; y++) {
-				materials [x, y] = MakeMaterial (x, y);
-			}
-		}
-	}
+    static Materials()
+    {
+        materials = new Material[numberOfAngles, numberOfAngles];
+        for (int x = 0; x < numberOfAngles; x++) {
+            for (int y = 0; y < numberOfAngles; y++) {
+                materials[x, y] = MakeMaterial(x, y);
+            }
+        }
+    }
 
-	public Material GetMaterial (int x, int y)
-	{
-		return materials [x, y];
-	}
-    
-	private Material MakeMaterial (int indexX, int indexY)
-	{
-		Material mat = new Material (Shader.Find (shader));
-		mat.SetTexture ("_MainTex", (Texture)Resources.Load ("GeneratedTextures/texture" + indexX + "_" + indexY));
-		mat.SetTexture ("_BumpMap", (Texture)Resources.Load ("GeneratedTextures/normal" + indexX + "_" + indexY));
+    public static Material GetMaterial(int x, int y)
+    {
+        return materials[x, y];
+    }
 
-		return mat;
-	}
+    private static Material MakeMaterial(int indexX, int indexY)
+    {
+        Material mat = new Material(Shader.Find(shader));
+        mat.SetTexture("_MainTex", (Texture)Resources.Load("GeneratedTextures/texture" + indexX + "_" + indexY));
+        mat.SetTexture("_BumpMap", (Texture)Resources.Load("GeneratedTextures/normal" + indexX + "_" + indexY));
+
+        return mat;
+    }
 }
